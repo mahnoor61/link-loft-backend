e56d32df99bf00763f656d35e23ca914df575642
muteedwaheed <55345658+mahnoor61@users.noreply.github.com>
2025-10-14 08:16:39 +0500

    c
    c

--- controllers/login_with_google.js
+++ controllers/login_with_google.js
@@ -3,12 +3,26 @@ const {success_response, error_response} = require('../utils/response');
 const jwt = require('jsonwebtoken');
 exports.signIn_with_google = async (req, res) => {
     try {
-        const {email, username, emailVerified, signup_method} = req.body;
+        let {email, username, emailVerified, signup_method, profile_photo} = req.body;
+        if (!email) {
+            return error_response(res, 400, 'Email is required');
+        }
+        email = String(email).toLowerCase();
+        if (!signup_method) signup_method = 'google';
         const checkUser = await User.findOne({email});
         if (checkUser) {
             // Update username if not set yet and a username is provided
             if (!checkUser.username && username) {
                 checkUser.username = username;
+            }
+            // Update profile photo if not set
+            if (!checkUser.profile_photo && profile_photo) {
+                checkUser.profile_photo = profile_photo;
+            }
+            // Record signup method and email verification state
+            if (signup_method) checkUser.signup_method = signup_method;
+            if (typeof emailVerified === 'boolean') {
+                checkUser.is_verified = emailVerified;
             }
             const payload = {user_id: checkUser._id, user_email: checkUser.email}
             const jwtToken = jwt.sign(payload, process.env.TOKEN_KEY, {
@@ -22,7 +36,8 @@ exports.signIn_with_google = async (req, res) => {
             signup_method,
             email,
             username: username,
-            is_verified: emailVerified
+            is_verified: !!emailVerified,
+            profile_photo: profile_photo || ''
         });
         const payload = {user_id: newUser._id, user_email: newUser.email}
         const jwtToken = jwt.sign(payload, process.env.TOKEN_KEY, {
